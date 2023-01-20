0ef96107fd033c13d927006065d3d97ce70e205e
Huncho <huncho@ghostsystems.io>
2023-01-20 02:07:30 -0800

    uncommenting

--- install.sh
+++ install.sh
@@ -1,15 +1,15 @@
 cp -r . ~/.ghost
 cd ~
 pwd
-#cp -r .ghost/ghost.vim .vim
-#echo 'source ~/.ghost/ghost.sh' >> .bashrc
-#cp .vimrc /tmp/.vimrc && echo 'source ~/.ghost/ghost.vimrc' > .vimrc && cat /tmp/.vimrc >> .vimrc
-#mkdir -p .vim/pack/vendor/start
-#cd .vim/pack/vendor/start
-#git clone https://github.com/preservim/nerdtree
-#git clone https://github.com/ycm-core/YouCompleteMe
-#git clone https://github.com/rafi/awesome-vim-colorschemes
-#git clone https://github.com/davidhalter/jedi-vim
-#git clone https://github.com/ryanoasis/vim-devicons
-#cd YouCompleteMe
-#./install.py --all
+cp -r .ghost/ghost.vim .vim
+echo 'source ~/.ghost/ghost.sh' >> .bashrc
+cp .vimrc /tmp/.vimrc && echo 'source ~/.ghost/ghost.vimrc' > .vimrc && cat /tmp/.vimrc >> .vimrc
+mkdir -p .vim/pack/vendor/start
+cd .vim/pack/vendor/start
+git clone https://github.com/preservim/nerdtree
+git clone https://github.com/ycm-core/YouCompleteMe
+git clone https://github.com/rafi/awesome-vim-colorschemes
+git clone https://github.com/davidhalter/jedi-vim
+git clone https://github.com/ryanoasis/vim-devicons
+cd YouCompleteMe
+./install.py --all
