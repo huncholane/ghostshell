12ef6687ac7dac761bc9aea68a8b0c0f30017588
Huncho <huncho@ghostsystems.io>
2023-01-20 01:32:03 -0800

    update readme

--- install.sh
+++ install.sh
@@ -5,10 +5,10 @@ echo 'source ~/.ghost/ghost.sh' >> .bashrc
 cp .vimrc /tmp/.vimrc && echo 'source ~/.ghost/ghost.vimrc' > .vimrc && cat /tmp/.vimrc >> .vimrc
 mkdir -p .vim/pack/vendor/start
 cd .vim/pack/vendor/start
-git clone https://github.com/perservim/nerdtree
+git clone https://github.com/preservim/nerdtree
 git clone https://github.com/ycm-core/YouCompleteMe
 git clone https://github.com/rafi/awesome-vim-colorschemes
 git clone https://github.com/davidhalter/jedi-vim
-git clone https://github.com/ryanoasis/vim-divicons
+git clone https://github.com/ryanoasis/vim-devicons
 cd YouCompleteMe
 ./install.py --all
